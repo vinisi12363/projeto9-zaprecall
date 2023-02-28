0ed20a295a8a039bf6959d65ccd9232c395f13df
vinisi12363 <viniciuspv.si@gmail.com>
2023-02-28 20:31:15 -0300

    adicionando imagens e css iniciais

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "../src/assets/images/logo.png"
 
-function App() {
+
+
+
+export default function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div className="main">
+        <div className="topIcons">
+          <img src="logo"></img>
+        </div>
+        <div className="cardsArea">
+          <div className="card"><p>pergunta1</p></div>
+          <div className="card"><p>pergunta1</p></div>
+          <div className="card"><p>pergunta1</p></div>
 
-export default App;
+        </div>
+        <div className="footer"></div>
+      </div>
+  )
+}
