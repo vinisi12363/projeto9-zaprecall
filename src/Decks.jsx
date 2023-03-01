d6272f913e86e5e90c91b11944e16da20333ac86
vinisi12363 <viniciuspv.si@gmail.com>
2023-03-01 14:26:43 -0300

    adicionando imagens e css iniciais

--- src/Decks.jsx
+++ src/Decks.jsx
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
+import seta from './assets/images/seta_play.png'
+import setaVirar from './assets/images/seta_virar.png'
+export default function Decks() {
 
-export default function  Decks(){
-   
-   
+
     return (
 
         <div className="Decks">
@@ -17,13 +18,38 @@ export default function  Decks(){
 function FlashCards() {
     return (
         <>
-            <div className="FlashCards"><p>pergunta1</p></div>
-            <div className="FlashCards"><p>pergunta1</p></div>
-            <div className="FlashCards"><p>pergunta1</p></div>
-            <div className="FlashCards"><p>pergunta1</p></div>
-           
-           
-            
+
+            <div className='flashCards hide'>
+                <p>pergunta 1</p>
+                <button></button><img src={seta}></img>
+            </div>
+                 <img src={setaVirar} id="setaVirar"></img>
+                <div className='flip-container'>
+                    <div className='flipper'>
+                        <div className="questionArea">
+                            <p>O que é JSX ?</p>
+                            
+                        </div>
+
+                        <div className='flashVerse'>
+                            <div className='responseArea'>
+                                <p>JSX é uma sintaxe dentro do JS  </p>
+                            </div>
+                            <div className='btnArea'>
+                                <button className="btnErrou">Não Lembrei</button>
+                                <button className="btnQuaseAcertou">Quase Lembrei</button>
+                                <button className='btnAcertou'>Zap!</button>
+                            </div>
+                        </div>
+                    </div>
+                 
+
+                </div>
+               
+
+
+
+
         </>
 
     )
